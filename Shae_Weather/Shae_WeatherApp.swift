f235788d12d46827282970e8ebb5a47d4c955a27
Shae Simeoni <shaebsimeoni@gmail.com>
2021-11-10 13:42:41 -0500

    Initial Commit

---
+++ Shae_Weather/Shae_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shae_WeatherApp.swift
+//  Shae_Weather
+//
+//  Created by Shae Simeoni on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Shae_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
